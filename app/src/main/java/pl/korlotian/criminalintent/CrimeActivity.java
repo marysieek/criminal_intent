a0ea26d3b8e625ba9bf376a622c3bd6a35e38fb4
Maria Stokłosa <maria.stoklosa@netguru.pl>
2016-12-28 22:13:41 +0100

    Get rid of unused imports

--- app/src/main/java/pl/korlotian/criminalintent/CrimeActivity.java
+++ app/src/main/java/pl/korlotian/criminalintent/CrimeActivity.java
@@ -1,9 +1,6 @@
 package pl.korlotian.criminalintent;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.os.Bundle;
 
 public class CrimeActivity extends SingleFragmentActivity {
 
