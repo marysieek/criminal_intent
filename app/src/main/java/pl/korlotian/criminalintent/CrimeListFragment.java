08b804c613433d10e83d6b572fc18a03c3953853
Maria Stokłosa <maria.stoklosa@netguru.pl>
2016-12-28 20:41:05 +0100

    Add updateUI method

--- app/src/main/java/pl/korlotian/criminalintent/CrimeListFragment.java
+++ app/src/main/java/pl/korlotian/criminalintent/CrimeListFragment.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class CrimeListFragment extends Fragment {
 
     private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,6 +27,14 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return view;
+    }
+
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+
+        mAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder {
