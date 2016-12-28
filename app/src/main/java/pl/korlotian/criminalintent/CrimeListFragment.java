01b7532bb6e8e482e4d2e34ef48567e720d0fb43
Maria Stokłosa <maria.stoklosa@netguru.pl>
2016-12-28 20:27:43 +0100

    Add view holder to CrimeHolder

--- app/src/main/java/pl/korlotian/criminalintent/CrimeListFragment.java
+++ app/src/main/java/pl/korlotian/criminalintent/CrimeListFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class CrimeListFragment extends Fragment {
@@ -23,5 +24,16 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return view;
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTitleTextView;
+
+        public CrimeHolder(View itemView) {
+            super(itemView);
+
+            mTitleTextView = (TextView) itemView;
+        }
     }
 }
