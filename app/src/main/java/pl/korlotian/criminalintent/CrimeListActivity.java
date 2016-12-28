5dca063ff356e2db2d48767e90b34b15a0f38c7b
Maria Stokłosa <maria.stoklosa@netguru.pl>
2016-12-28 19:57:51 +0100

    Create CrimeListActivity and Fragment classes

---
+++ app/src/main/java/pl/korlotian/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package pl.korlotian.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
