940fe55b6b7e3366370879cafd43fa7bf2d584fa
Maria Stokłosa <maria.stoklosa@netguru.pl>
2016-12-30 16:10:04 +0100

    Add CrimePagerActivity with activity_crime_pager layout

---
+++ app/src/main/java/pl/korlotian/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,14 @@
+package pl.korlotian.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class CrimePagerActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+    }
+}
