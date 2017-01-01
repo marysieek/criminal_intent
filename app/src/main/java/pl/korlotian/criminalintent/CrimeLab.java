dcdbf3cd727db840a02da6814e9dd41f1aef62c6
Maria Stokłosa <maria.stoklosa@netguru.pl>
2017-01-01 13:49:55 +0100

    Create CrimeTable

--- app/src/main/java/pl/korlotian/criminalintent/CrimeLab.java
+++ app/src/main/java/pl/korlotian/criminalintent/CrimeLab.java
@@ -1,6 +1,7 @@
 package pl.korlotian.criminalintent;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,8 @@ public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
     private List<Crime> mCrimes;
+    private Context mContext;
+    private SQLiteDatabase mDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -17,7 +20,11 @@ public class CrimeLab {
         }
         return sCrimeLab;
     }
+
     private CrimeLab(Context context) {
+        mContext = context.getApplicationContext();
+        mDatabase = new CrimeBaseHelper(mContext)
+                .getWritableDatabase();
         mCrimes = new ArrayList<>();
     }
 
